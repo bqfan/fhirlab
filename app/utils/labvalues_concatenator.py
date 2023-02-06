18c4bf0626c8438aec27814062c600e472022ee2
Baiquan Fan <baiquan.fan@gamil.com>
2023-02-06 21:01:40 +0200

    add initial labvalues yaml files and concatenate them to json.

---
+++ app/utils/labvalues_concatenator.py
@@ -0,0 +1,13 @@
+import glob
+import yaml, json
+
+def concatenate(lab_name="default"):
+    filenames = glob.glob("../schemas/labvalues/" + lab_name + "/*.yaml")
+    labvalues={}
+
+    for file in filenames:
+        with open(file) as f:
+            labvalue = json.loads(json.dumps(yaml.full_load(f), sort_keys=True, indent=2))
+            labvalues = dict(labvalues, **labvalue)
+
+    return labvalues
