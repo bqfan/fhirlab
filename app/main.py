41e6cec660238bf6e5f7d992a0afb7745c28a1d3
Baiquan Fan <baiquan.fan@gamil.com>
2023-03-16 20:32:12 +0200

    load labvalues as final constant when starting app.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from typing import Final
 from app.utils import labvalues_concatenator
 # from fastapi.middleware.cors import CORSMiddleware
 
@@ -26,7 +27,7 @@ app = FastAPI()
 # app.include_router(user.router)
 # app.include_router(auth.router)
 # app.include_router(vote.router)
-
+LABVALUES: Final[dict] = labvalues_concatenator.concatenate("default")
 
 @app.get("/v1/labvalues/")
 def root():
@@ -35,8 +36,9 @@ def root():
 
 @app.get("/v1/labvalues/{id}")
 def labvalue_by_id(id):
+    global labvalues
     try:
-        labvalue = labvalues_concatenator.concatenate("default")[id]
+        labvalue = LABVALUES[id]
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"lab value: {id} does not exist")
