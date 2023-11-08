3049b2a399efd83ca5810b7ac6c4f9227f1d82ab
Baiquan Fan <baiquan.fan@outlook.com>
2023-11-08 20:39:16 +0200

    make resources folders logic.

--- app/utils/reference_loader.py
+++ app/utils/reference_loader.py
@@ -35,7 +35,7 @@ def get_reference_keys(references: dict) -> dict:
     return keys
 
 def cancatenate_yaml_files(lab_name: str) -> str:
-    filenames = glob.glob("app/resources/references/" + lab_name + "/observations/*.yaml")
+    filenames = glob.glob(f"app/resources/observations/{lab_name}/reference_ranges/*.yaml")
     # filenames = glob.glob("app/resources/references/" + lab_name + "/observations/*.yaml") + \
     #     glob.glob("app/resources/references/" + lab_name + "/bundles/*.yaml") + \
     #         glob.glob("app/resources/references/" + lab_name + "/acronyms/*.yaml") 
