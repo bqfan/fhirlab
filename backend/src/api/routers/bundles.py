1d870e4f3ffff53b1f9115542940223be3d8879c
Baiquan Fan <baiquan.fan@outlook.com>
2024-01-10 18:04:49 +0200

    add routers.

---
+++ backend/src/api/routers/bundles.py
@@ -0,0 +1,52 @@
+from fastapi import Body, status, HTTPException, Depends, Security
+from fastapi import APIRouter
+from fastapi.security.api_key import APIKey
+from backend.src.api.resources.resource_loader import Resource
+from backend.src.api.models.schemas.references import Bundle, TempEnum
+from backend.src.api.utils import API_KEY, get_api_key, __bundle_formatter
+
+router = APIRouter(
+    prefix="/Bundles",
+    tags=['Bundles']
+)
+
+resource = Resource().load()
+
+reference_keys = {}
+for key in resource.reference_keys:
+    reference_keys[key] = key
+
+ReferenceKeys = TempEnum("ReferenceKeys", reference_keys)
+
+bundle_keys = {}
+for key in resource.bundle_keys:
+    bundle_keys[key] = key
+
+BundleKeys = TempEnum("BundleKeys", bundle_keys)
+
+acronym_keys = {}
+for key in resource.acronyms:
+    acronym_keys[key] = key
+
+AcronymKeys = TempEnum("BundleKeys", acronym_keys)
+
+@router.get("/_references", summary="Returns all bundle references", status_code=status.HTTP_200_OK, tags=["Bundles"])
+def get_bundles(api_key: APIKey = Depends(get_api_key)) -> dict:
+    __bundle_formatter(resource)
+
+    return resource.bundles
+
+@router.get("/_keys", summary="Returns all bundle keys", tags=["Bundles"])
+def get_bundle_keys(api_key: APIKey = Depends(get_api_key)) -> list:
+    return resource.bundle_keys
+
+@router.get("/{key}/_references", summary="Returns a bundle's references by bundle key", status_code=status.HTTP_200_OK, response_model=Bundle, response_model_exclude_unset=True, tags=["Bundles"])
+def get_bundle_by_key(key: BundleKeys, api_key: APIKey = Depends(get_api_key)):
+    try:
+        __bundle_formatter(resource)
+        bundle = resource.bundles[key]
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"bundle key {key} not found")
+
+    return bundle
---
+++ backend/src/api/utils.py
@@ -0,0 +1,63 @@
+from fastapi import Security, HTTPException
+from fastapi.security import APIKeyHeader
+import json
+
+API_KEY = "123" #settings.123
+API_KEY_NAME = "Authorization"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+
+async def get_api_key(
+    api_key_header: str = Security(api_key_header),
+):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(status_code=403)
+
+def __bundle_formatter(resource):
+    for _, value in resource.bundles.items():
+        for entry in value['entry']:
+            entry['fullUrl'] = entry['fullUrl'].replace('BaseUrl', resource.base_url)
+            if isinstance(entry['resource'], str):
+                entry['resource'] = resource.references[entry['resource']]
+
+def get_json(obj):
+  return json.loads(
+    json.dumps(obj, default=lambda o: getattr(o, '__dict__', str(o)))
+  )
+
+def __check_semantic_interoperable(observation, reference):
+    reference_code = reference["code"]
+    observation_code = observation["code"]
+
+    for observation_code_coding in observation_code["coding"]:
+        for reference_code_coding in reference_code["coding"]:
+            if observation_code_coding["system"] == reference_code_coding["system"] and \
+                observation_code_coding["code"] == reference_code_coding["code"]:
+
+                return True
+    
+    return False
+
+def __check_unit(observation, reference):
+    reference_ranges = reference["referenceRange"]
+    observation_value_quantity = observation["valueQuantity"]
+
+    for reference_range in reference_ranges:
+        if "high" in reference_range and "low" in reference_range and \
+                observation_value_quantity["system"] == reference_range["high"]["system"] and \
+                    observation_value_quantity["unit"] == reference_range["high"]["unit"] and \
+                        observation_value_quantity["system"] == reference_range["low"]["system"] and \
+                            observation_value_quantity["unit"] == reference_range["low"]["unit"]:
+                                return True
+        elif "high" in reference_range and "low" not in reference_range and \
+                observation_value_quantity["system"] == reference_range["high"]["system"] and \
+                    observation_value_quantity["unit"] == reference_range["high"]["unit"]:
+                        return True
+        elif "high" not in reference_range and "low" in reference_range and \
+                observation_value_quantity["system"] == reference_range["low"]["system"] and \
+                    observation_value_quantity["unit"] == reference_range["low"]["unit"]:
+                        return True
+
+        else:
+            return False
